319284ef66c21aef2d957f10a67f22b10048d9a5
Jean Landim <jewanbb@gmail.com>
2020-02-27 10:24:37 -0300

    updating

--- update.sh
+++ update.sh
@@ -6,10 +6,11 @@ DOTFILES_DIR=$HOME/Projetos/dotfiles/
 cd $HOME
 # save files 
 for f in $FILES; do
-  cp $FILES -v $DOTFILES_DIR
+  cp $f -v $DOTFILES_DIR
 done
 
 # save gnome terminal settings
+echo 'saving gnome-terminal settings'
 dconf dump /org/gnome/terminal/ > $DOTFILES_DIR/gnome_terminal
 
 
